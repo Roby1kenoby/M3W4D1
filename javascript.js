5aa2d9a430c4ddb1d7721019a72965d9b6011b39
Roberto Allocco <r.allocco@pragmos.it>
2024-05-11 17:12:01 +0200

    Added Responsiveness

--- javascript.js
+++ javascript.js
@@ -1,15 +1,10 @@
-let scrollPosition = 0;
 // array con le section
 let sections = document.getElementsByTagName("section")
-
-// function logScrollPosition(scrollPos){
-//     console.log(scrollPos)
-// }
 
 document.addEventListener("scroll", (event) => {
     // per ogni sezione, sull'evento di scroll
     for (let s of sections){
-        // verifico se la posizione del lato basso del rettangolo è oltre il bordo basso dello schermo
+        // verifico se la posizione del lato alto del rettangolo è oltre il bordo basso dello schermo
         if(s.getBoundingClientRect().top >= window.innerHeight){
             // se si, allora quella sezione deve essere invisibile
             s.classList.add("invisible")
